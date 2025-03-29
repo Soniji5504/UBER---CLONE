d62a26220fc52efea0ce07e683d7dabdf9479450
soniji5504 <dhruvsoniji@050504@gmail.com>
2025-03-29 09:22:40 +0530

    add frontend setup with Tailwind CSS, Vite, and initial pages for user and captain authentication

---
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const CaptainLogin = () => {
+  return (
+    <div >CaptainLogin </div>
+  )
+}
+export default CaptainLogin
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div >
+        <div className='h-screen w-full flex justify-between flex-col bg-red-400'>
+            <img className='h-30' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"alt="" />
+            <div classanme= 'bg-white'>
+                <h2>Get started with UBER</h2>
+                <button>Continue</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Home
